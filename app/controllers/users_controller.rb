9900d54e2adc3d6ae0ad85c979e0ace8cc96d3c0
kosuke haruna <haruenginner@gmail.com>
2018-02-21 08:01:11 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
-  def new
+  def show
+   @user = User.find(params[:id])
   end
+  # => app/views/users/show.html.erb
+  def new
+   @user = User.new
+  end
+  
+  def create
+   @user = User.new(user_params)
+   if @user.save # => validation
+    flash[:success] = "Welcome to the Sample App!"
+    redirect_to @user
+    # Sucess
+   else
+    # Failure
+    render 'new'
+   end
+  end
+   
+    private
+    
+   def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+   end
 end
